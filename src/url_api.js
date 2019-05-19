65dffcea42efc017acb0694e5e31e55070743fca
Yan Tadeu <yan.tadeu1@gmail.com>
2019-05-18 21:09:48 -0300

    Drop image

--- src/url_api.js
+++ src/url_api.js
@@ -1,9 +1,9 @@
 const dev={
-    API_URL:"http://localhost:300"
+    API_URL:"http://localhost:3000"
 };
 
 const prod={
-    API_URL:"llll"
+    API_URL:"https://yanmemegeneratorserver.herokuapp.com"
 };
 
 const url_api = process.env.NODE_ENV === 'development' ? dev : prod;
